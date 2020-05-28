0febcf264a4c02ff9ad5c7fb5ea306ac259e64df
MNeluG <62048952+MNeluG@users.noreply.github.com>
2020-05-28 16:42:27 +0300

    Update remove-from-cart.php
    Am refacut scriptul ca  stergerea produselor din cos sa fie individual oentru fiecare client in parte si sa salveze la fel pentru fiecare user autentificat in parte.

--- MyCart/remove-from-cart.php
+++ MyCart/remove-from-cart.php
@@ -2,9 +2,9 @@
 
 //---pentru stergerea produselor din cos---
 
-foreach($_SESSION["mycart"] as $keys => $values){  
+foreach($_SESSION[$session_now] as $keys => $values){  
     if($values["product-id"] == $_GET["id"]){  
-        unset($_SESSION["mycart"][$keys]);  
+        unset($_SESSION[$session_now][$keys]);  
         echo '<script>alert("Item Removed")</script>';  
         echo '<script>window.location="mycart.php"</script>';  
     }  
