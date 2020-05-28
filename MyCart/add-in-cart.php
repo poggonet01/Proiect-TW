fd0fda03d21e7b4d96d06926d9fb3322b6a4d5f7
MNeluG <62048952+MNeluG@users.noreply.github.com>
2020-05-28 16:38:45 +0300

    Update add-in-cart.php
    Am actualizat scriptul de adaugare si salvare a produselor in cos,scriptul creeaza si salveaza cosul pentru fiecare user autentificat in parte,chiar dupa deautentificare cosul ramane salvat pentru fiecare utilizator.

--- MyCart/add-in-cart.php
+++ MyCart/add-in-cart.php
@@ -1,23 +1,22 @@
 <?php 
-    if(isset($_SESSION["mycart"])){
+    if(isset($_SESSION[$session_now])){
         //---creem un array ce contine id-urile produselor---
-        $items_id=array_column($_SESSION["mycart"], "product-id");
+        $items_id=array_column($_SESSION[$session_now], "product-id");
         //---in caz de produdul nu e adaugat in cos inseram datele din tabel despr acesta---
         if(!in_array($_GET["id"], $items_id))  
         {  
-             $count = count($_SESSION["mycart"]);  
+             $count = count($_SESSION[$session_now]);  
              $items = array(  
                 'product-id'       =>     $_GET["id"],  
                 'product-name'     =>     $_POST["hidden_name"],  
                 'product-price'    =>     $_POST["hidden_price"],  
                 'product-quantity' =>     $_POST["quantity"]  
            );
-             $_SESSION["mycart"][$count] = $items;  
+             $_SESSION[$session_now][$count] = $items;  
         }  
         else 
         //---cazul in care produsul e deja adaugat---
         {  
-             echo '<script>alert("Produsul a fost deja adaugat")</script>';  
              echo '<script>window.location="mycart.php"</script>';  
         } 
         
@@ -29,6 +28,6 @@
             'product-price'    =>     $_POST["hidden_price"],  
             'product-quantity' =>     $_POST["quantity"]  
        );
-       $_SESSION["mycart"][0]=$items;
+       $_SESSION[$session_now][0]=$items;
     }
 ?>
